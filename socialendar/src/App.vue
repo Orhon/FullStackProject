b148dc2fda5bab0f652c2c21dfc6a1736c4908bf
Arjan West <arjan.west@student.howest.be>
2018-11-20 15:23:28 +0100

    homepage -> home, ITCSS structuur aangepast

--- socialendar/src/App.vue
+++ socialendar/src/App.vue
@@ -1,37 +1,31 @@
 <template>
   <div id="app">
-    <homepage></homepage>
+    <home/>
   </div>
 </template>
 
 <script>
-import Homepage from '@/views/Homepage.vue'
+import home from '@/views/Home.vue'
 
 export default {
   name: "app",
   components: {
-    Homepage
+    home
   }
 }
 </script>
 
 
 <style lang="scss">
+
+@import "./src/style/base";
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- socialendar/src/views/Home.vue
+++ socialendar/src/views/Home.vue
@@ -1,21 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <div><router-link to="/home">Home</router-link> |</div>
-      <div><router-link to="/calendar">Calendar</router-link> |</div>
+      <div v-if="!loggedIn"><router-link to="/home">Home</router-link> |</div>
       <div v-if="!loggedIn"><router-link   to="/login">Login </router-link> |</div>
       <div  v-if="!loggedIn"><router-link  to="/register">Register</router-link> |</div>
       <div v-if="loggedIn"><router-link  to="/logout">Logout</router-link></div>  
-   
     </div>
     <router-view/>
-
+    
 
 
   </div>
 </template>
 
-<script>
+<script>  
 // @ is an alias to /src
 
 export default {
