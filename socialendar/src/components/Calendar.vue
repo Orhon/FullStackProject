6ca59403f8e0b2e450a83ae7b096f8576aa37ad2
Arjan West <arjan.west@student.howest.be>
2018-11-29 20:54:50 +0100

    Login & register pagina opmaak

--- socialendar/src/components/Calendar.vue
+++ socialendar/src/components/Calendar.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="c-calendar">
-    <div v-for="ev in this.GetAllEvents" :key="ev.title">
-      <calendar-item   :event="ev"/>
-    </div>
+      <calendar-item v-for="ev in this.GetAllEvents" :key="ev.title" :event="ev"/>
     
 
   </div>
@@ -15,13 +13,9 @@ export default {
   components:{
     CalendarItem
   },
-  created: function(){
-      console.log(this.$store.getters.getsavedEvents)
-  },
   computed:{
       GetAllEvents () {
             return this.$store.getters.getsavedEvents
-            console.log(this.$store.getters.getsavedEvents)
         }
   },
   
--- socialendar/src/components/CalendarItem.vue
+++ socialendar/src/components/CalendarItem.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="c-calendar__event">
-   <!-- <h2 class="c-calendar-event__title">{{event.title}}</h2> -->
+   <h2 class="c-calendar-event__title">{{event.title}}</h2>
     <p class="c-calendar-event__description">{{event.description}}</p>
+    <h3 class="c-calendar-event__period">Location</h3>
     <p class="c-calendar-event__description">{{event.location}}</p>
     <h3 class="c-calendar-event__period">Start</h3>
-    <!-- <p class="c-calendar-event__date">{{c.date}}</p> -->
+    <p class="c-calendar-event__date">{{event.date}}</p>
   </div>
 </template>
 
@@ -19,7 +20,10 @@ export default {
     props:{
             event:{
                 description: String,
-                location: String
+                location: String,
+                title: String,
+                time: Object,
+                date: String,
             }
     },
     created(){
